1f92286d9c4dfb97ebb78df0e44ee6874aaca967
rtobin <ryantobin248@gmail.com>
2015-10-22 17:13:18 -0400

    views for band.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   helper_method :current_user
+  helper_method :logged_in?
 
   def current_user
     return nil unless self.session[:session_token]
