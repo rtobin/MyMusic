044391beec586f857fae8400d5804c264fb9cbbd
rtobin <ryantobin248@gmail.com>
2015-10-22 11:57:59 -0400

    sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,30 @@
 class SessionsController < ApplicationController
+
+  def new
+    # go to login
+    render :new
+  end
+
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:email],
+      params[:user][:password]
+    )
+
+    unless @user
+      # bad credentials!
+      # back to login
+      render :new
+    else
+      # good credentials!
+      # sign the user in
+      log_in!(@user) # new session_token
+      redirect_to user_url
+    end
+  end
+
+  def destroy
+    log_out! # kills session_token
+    redirect_to new_session_url
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,28 @@ class UsersController < ApplicationController
 
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in!(@user)
+      redirect_to user_url
+    else
+      render :new
+    end
   end
 
   def new
+    @user = User.new
+    render :new
   end
 
   def show
+    unless current_user
+      redirect_to new_session_url
+      return
+    end
+
+    @user = current_user
+    render :show
   end
 
   private
