5a9b4caed7238ea1a3b0be03d617693d6cd821f8
rtobin <ryantobin248@gmail.com>
2015-10-22 22:31:46 -0400

    Notes migration and model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151023020718) do
+ActiveRecord::Schema.define(version: 20151023022845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,14 @@ ActiveRecord::Schema.define(version: 20151023020718) do
 
   create_table "bands", force: :cascade do |t|
     t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "notes", force: :cascade do |t|
+    t.integer  "track_id",   null: false
+    t.integer  "user_id",    null: false
+    t.text     "content",    null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
